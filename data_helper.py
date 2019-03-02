4d35272d17f18bd0dd6e8e398800648708f97032
Joon <joonable2@gmail.com>
2019-03-02 15:27:45 +0900

    initial commit

---
+++ data_helper.py
@@ -0,0 +1,180 @@
+import pandas as pd
+import re
+
+act_cate_list = ['고객 Outbound', '모바일 Outbound', 'SMS발송', '메일 Outbound', 'Email - Outbound', '기타 Outbound',
+                 '채널 자동답변(OB)']
+sr_act_dict = {}
+
+def raw_file_to_df(file_path):
+    df = pd.read_csv(file_path, sep='\t')
+
+    #TODO 모든 column명 특히 한글에 대응하도록 reindex로 추후 바꿔야함
+    df.columns = ['sr_no', 'sr_area', 'sr_channel', 'sr_cate_b', 'sr_cate_m', 'sr_cate_s', 'sr_text',
+                  'act_no', 'act_text', 'act_status', 'act_cate', 'supp_ques',
+                  'ord_cd', 'ord_status', 'ord_date', 'supp_cd', 'supp_nm', 'prd_cd', 'prd_nm', 'prd_desc']
+
+    df = df[['sr_no', 'sr_area', 'sr_channel', 'sr_cate_b', 'sr_cate_m', 'sr_cate_s', 'sr_text', 'ord_status',
+             'act_no', 'act_text', 'act_status', 'act_cate', 'supp_ques',
+             'prd_cd', 'prd_nm', 'prd_desc', 'ord_cd', 'ord_date', 'supp_cd', 'supp_nm']]
+
+    df['sr_no'] = df['sr_no'].apply(lambda string: _remove_single_quotation(string))
+    df['sr_area'] = df['sr_area'].apply(lambda string: _remove_single_quotation(string))
+    df['sr_channel'] = df['sr_channel'].apply(lambda string: _remove_single_quotation(string))
+    df['sr_cate_b'] = df['sr_cate_b'].apply(lambda string: _remove_single_quotation(string))
+    df['sr_cate_m'] = df['sr_cate_m'].apply(lambda string: _remove_single_quotation(string))
+    df['sr_cate_s'] = df['sr_cate_s'].apply(lambda string: _remove_single_quotation(string))
+    df['sr_text'] = df['sr_text'].apply(lambda string: _remove_single_quotation(string))
+    #TODO ord_status call sr의 경우에는 뽑는 방식으로 변경
+    df['ord_status'] = df['ord_status'].apply(lambda string: _remove_single_quotation(string))
+    df['act_no'] = df['act_no'].apply(lambda string: _remove_single_quotation(string))
+    df['act_text'] = df['act_text'].apply(lambda string: _remove_single_quotation(string))
+    df['act_status'] = df['act_status'].apply(lambda string: _remove_single_quotation(string))
+    df['act_cate'] = df['act_cate'].apply(lambda string: _remove_single_quotation(string))
+    df['prd_nm'] = df['prd_nm'].apply(lambda string: _remove_single_quotation(string))
+    df['prd_desc'] = df['prd_desc'].apply(lambda string: _remove_single_quotation(string))
+    df['ord_date'] = df['ord_date'].apply(lambda string: _remove_single_quotation(string))
+    df['supp_nm'] = df['supp_nm'].apply(lambda string: _remove_single_quotation(string))
+    df['supp_ques'] = df['supp_ques'].apply(lambda string: _remove_single_quotation(string))
+
+    #TODO 활동번호가 아니라 활동시간으로 변경
+    df = df.sort_values(['sr_no', 'act_no']).reset_index(drop=True)
+
+    df['sr_start_type'] = df.sr_text.apply(lambda x: _sr_start_type(x))
+
+    df["customer_text"] = ""
+    df["gs_text"] = ""
+
+    df = df.apply(lambda row: _extract_customer_text_from_question_ver3(row), axis=1)
+    df = df.apply(lambda row: _extract_gs_text_from_question_ver2(row, df), axis=1)
+    del sr_act_dict
+
+    df['customer_text'] = df.customer_text.apply(lambda x: _cleansing_raw_text_by_char_ver2(x))
+    df['gs_text'] = df.gs_text.apply(lambda x: _cleansing_raw_text_by_char_ver2(x))
+
+    return df
+
+
+def _remove_single_quotation(string):
+    new_string = string
+    try:
+        new_string = re.sub(pattern = "(^'|'$)", repl="", string=string)
+    except TypeError:
+        new_string = re.sub(pattern = "(^'|'$)", repl="", string=str(string))
+    finally:
+        return new_string.strip()
+
+
+def _check_start_with_ver2(x, compare_string_list: list):
+    if not x:
+        return False
+
+    for string in compare_string_list:
+        if x.startswith(string):
+            return True
+
+    return False
+
+
+def _sr_start_type(sr_text):
+    if not sr_text:
+        return False
+
+    if sr_text.startswith("@고객"):
+        return 'call_type_customer'
+
+    if sr_text.startswith("@GS"):
+        return 'call_type_gs'
+
+    if sr_text.startswith("@접수시간"):
+        return 'mobile_type'
+
+    return 'other_type'
+
+
+def _extract_customer_text_from_question_ver3(row):
+    string = row['sr_text']
+    if row['sr_start_type'] == 'call_type_customer':
+        try:
+            if '@GS' in string:
+                found = re.search(pattern='@고객(.+)@GS', string=string).group(1)
+            else:
+                found = re.search(pattern='@고객(.+)$', string=string).group(1)
+            row['customer_text'] = found
+        except AttributeError:
+            print("call : ", string)
+            row['customer_text'] = string
+
+    elif row['sr_start_type'] == 'call_type_gs':
+        try:
+            if '@고객' in string:
+                found = re.search(pattern='@고객(.+)$', string=string).group(1)
+            else:
+                found = ""
+            row['customer_text'] = found
+        except AttributeError:
+            print("call : ", string)
+            row['customer_text'] = string
+    elif row['sr_start_type'] == 'mobile_type':
+        try:
+            found = re.search(pattern='@(문의)*내용 : (.+?)$', string=string).group(2)
+            row['customer_text'] = found
+        except AttributeError:
+            print("mob : ", string)
+            row['customer_text'] = string
+    else:
+        #         print(row)
+        row['customer_text'] = string
+    return row
+
+
+def _extract_gs_text_from_question_ver2(row, df):
+    string = row['sr_text']
+    if row['sr_start_type'] == 'call_type_customer':
+        try:
+            if '@GS' in string:
+                found = re.search(pattern='@GS(.+)$', string=string).group(1)
+            else:
+                found = ""
+            row['gs_text'] = found
+        except AttributeError:
+            print("call : ", string)
+    elif row['sr_start_type'] == 'call_type_gs':
+        try:
+            if '@고객' in string:
+                found = re.search(pattern='@GS(.+)@고객', string=string).group(1)
+            else:
+                found = re.search(pattern='@GS(.+)$', string=string).group(1)
+            row['gs_text'] = found
+        except AttributeError:
+            print("call : ", string)
+    elif row['sr_start_type'] == 'mobile_type':
+        sr_no = row['sr_no']
+        if sr_no in sr_act_dict.keys():
+            row['gs_text'] = sr_act_dict[sr_no]
+        else:
+            df_temp = df.loc[df.sr_no == sr_no, ['act_no', 'act_cate', 'act_text']]
+            for i, v in df_temp.iterrows():
+                if v['act_cate'] in act_cate_list:
+                    row['gs_text'] = v['act_text']
+                    sr_act_dict[sr_no] = v['act_text']
+                    break
+            if row['gs_text'] == "":
+                idx = df_temp.index.tolist()[0]
+                row['gs_text'] = df_temp.loc[idx, 'act_text']
+                sr_act_dict[sr_no] =  df_temp.loc[idx, 'act_text']
+    return row
+
+
+def _cleansing_raw_text_by_char_ver2(text):
+    text = re.sub(pattern=r'\n|\t|ㆍ|\r|,|;|:|<|>|-|_|=|\+|/|@|\(|\)|\*|&|\^|#|\$|~|◈|★|■|●|\[|\]|\"', repl=" ", string=text)
+    text = re.sub(pattern=r'( )+', repl=' ', string=text)
+
+    text = re.sub(pattern=r'(\.)+', repl='.', string=text)
+    text = re.sub(pattern=r'(\?)+', repl='?', string=text)
+    text = re.sub(pattern=r'(\!)+', repl='!', string=text)
+    text = re.sub(pattern=r'(ㅎ)+', repl='ㅎ', string=text)
+    text = re.sub(pattern=r'(ㅋ)+', repl='ㅋ', string=text)
+    text = re.sub(pattern=r'(ㅠ|ㅜ)+', repl='ㅠ', string=text)
+    text = re.sub(pattern=r'(♡|♥)+', repl='♥', string=text)
+
+    return text
